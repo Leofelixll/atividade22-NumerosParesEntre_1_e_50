0c970062b07548bf18e79bde172299fc0998e056
Leofelixll <fleo16leo@gmail.com>
2023-10-31 16:22:46 -0300

    first commit

---
+++ atividade22.py
@@ -0,0 +1,4 @@
+# Exercício Python 22: Faça um programa que mostre na tela os numeros pares entre 1 e 50 (dica estude range e seus paramentros)
+
+for numero in range(0, 51, 2):
+    print(f'os numeros pares entre 1 e 50 são:{numero}')
